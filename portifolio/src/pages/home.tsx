27183dccf029fc2793f9411bb1f4c7eaaff99fa0
kuma <kumaleta2021@gmail.com>
2024-12-28 16:28:32 +0300

    add Hero ,About ,Projects to home page

--- portifolio/src/pages/home.tsx
+++ portifolio/src/pages/home.tsx
@@ -1,4 +1,13 @@
+import About from "../components/sections/about";
+import Hero from "../components/sections/hero";
+import Projects from "../components/sections/projects";
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Projects />
+    </div>
+  );
 };
 export default Home;
