5317fea6f42e42e52a9de547185112cc14ecb1db
kuma <kumaleta2021@gmail.com>
2024-12-28 18:05:39 +0300

    skills added to home

--- portifolio/src/pages/home.tsx
+++ portifolio/src/pages/home.tsx
@@ -1,12 +1,14 @@
 import About from "../components/sections/about";
 import Hero from "../components/sections/hero";
 import Projects from "../components/sections/projects";
+import Skills from "../components/sections/skills";
 const Home: React.FC = () => {
   return (
     <div>
       <Hero />
       <About />
       <Projects />
+      <Skills />
     </div>
   );
 };
