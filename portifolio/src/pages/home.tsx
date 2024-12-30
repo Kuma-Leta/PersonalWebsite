c02181af3fc5bb0db8674ce5746f985d51ce1c82
kuma <kumaleta2021@gmail.com>
2024-12-30 09:22:54 +0300

    add certifications section to home

--- portifolio/src/pages/home.tsx
+++ portifolio/src/pages/home.tsx
@@ -1,7 +1,9 @@
 import About from "../components/sections/about";
+import Certifications from "../components/sections/certifications";
 import Hero from "../components/sections/hero";
 import Projects from "../components/sections/projects";
 import Skills from "../components/sections/skills";
+import Testimonials from "../components/sections/testimonials";
 const Home: React.FC = () => {
   return (
     <div>
@@ -9,6 +11,8 @@ const Home: React.FC = () => {
       <About />
       <Projects />
       <Skills />
+      <Testimonials />
+      <Certifications />
     </div>
   );
 };
