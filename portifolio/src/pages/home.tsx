426b1e2e30ff2e2e6a3affb87d6a351cf73d4d58
kuma <kumaleta2021@gmail.com>
2024-12-30 20:34:11 +0300

    add Contact section to home

--- portifolio/src/pages/home.tsx
+++ portifolio/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import About from "../components/sections/about";
 import Certifications from "../components/sections/certifications";
+import Contact from "../components/sections/contact";
 import Experience from "../components/sections/experience";
 import Footer from "../components/sections/footer";
 import Hero from "../components/sections/hero";
@@ -16,6 +17,7 @@ const Home: React.FC = () => {
       <Experience />
       <Testimonials />
       <Certifications />
+      <Contact />
       <Footer />
     </div>
   );
