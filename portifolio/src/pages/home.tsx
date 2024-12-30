f1aa254b829850c03d1592a71f0b7708a9332176
kuma <kumaleta2021@gmail.com>
2024-12-30 15:15:37 +0300

    add Experience to the home

--- portifolio/src/pages/home.tsx
+++ portifolio/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import About from "../components/sections/about";
 import Certifications from "../components/sections/certifications";
+import Experience from "../components/sections/experience";
 import Hero from "../components/sections/hero";
 import Projects from "../components/sections/projects";
 import Skills from "../components/sections/skills";
@@ -11,6 +12,7 @@ const Home: React.FC = () => {
       <About />
       <Projects />
       <Skills />
+      <Experience />
       <Testimonials />
       <Certifications />
     </div>
