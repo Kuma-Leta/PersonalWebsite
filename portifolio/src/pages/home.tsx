cbcdf349c1a6d204bf64d6a7f16abf93e9394f01
kuma <kumaleta2021@gmail.com>
2024-12-31 15:35:36 +0300

    optimize seo for home.tsx

--- portifolio/src/pages/home.tsx
+++ portifolio/src/pages/home.tsx
@@ -7,9 +7,22 @@ import Hero from "../components/sections/hero";
 import Projects from "../components/sections/projects";
 import Skills from "../components/sections/skills";
 import Testimonials from "../components/sections/testimonials";
+import { Helmet } from "react-helmet";
 const Home: React.FC = () => {
   return (
     <div>
+      <Helmet>
+        <title>Kuma Leta - Portfolio</title>
+        <meta
+          name="description"
+          content="Get in touch with me through my contact form. Email me at kumaleta2021@gmail.com or visit me at https://linkedin.com/in/kumaleta"
+        />
+        <meta
+          name="keywords"
+          content="contact, email, social media, address, portfolio"
+        />
+        <meta name="author" content="Kuma Leta" />
+      </Helmet>
       <Hero />
       <About />
       <Projects />
